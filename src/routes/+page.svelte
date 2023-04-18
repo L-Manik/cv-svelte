118aa623caef313e87042696aa76272552772389
L-Manik <124031190+L-Manik@users.noreply.github.com>
2023-04-18 08:43:52 +0000

    Customized front page and added routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,6 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p> This is the pilot run of using Svelte as code framework.
 </p>
+
+<a href="/testRoute">Enter Test Route?</a>
+<a href="https://youtube.com">Go to Youtube?</a>
---
+++ src/routes/testRoute/deeperRoute/+page.svelte
@@ -0,0 +1,2 @@
+<h1>What are you doing? This is an even deeper route.</h1>
+<a href="/testRoute">Out the rabbit hole?</a>
