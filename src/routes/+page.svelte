de042256025f209d308f0c3b5ad31ebddb256ccd
L-Manik <124031190+L-Manik@users.noreply.github.com>
2023-04-18 08:26:03 +0000

    set up npm run dev

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit Initial Run</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p> This is the pilot run of using Svelte as code framework.
+</p>
