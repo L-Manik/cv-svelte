9b67f0155b31f3e47b7599152ce7358d57b44fcd
L-Manik <124031190+L-Manik@users.noreply.github.com>
2023-04-18 08:47:34 +0000

    Finalization for Day 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <h1>Welcome to SvelteKit Initial Run</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p> This is the pilot run of using Svelte as code framework.
-</p>
+<p>This is the pilot run of using Svelte as code framework.</p>
 
-<a href="/testRoute">Enter Test Route?</a>
+<a href="/testRoute">Enter Test Route?</a><br>
 <a href="https://youtube.com">Go to Youtube?</a>
