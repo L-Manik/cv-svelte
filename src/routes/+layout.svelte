bb2e1f5b3f65908450cec5ca3a596dd6f1fc6850
L-Manik <124031190+L-Manik@users.noreply.github.com>
2023-05-02 07:57:34 +0000

    sidebar exercise

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<div class="parent">
+    <div class="sidebar"/>
+    <div class="content">
+    <slot/>
+    </div>
+</div>
+<style>
+    :global(html, body){
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+    }
+    .parent {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+    }
+    .sidebar {
+        background-color:rgb(11, 152, 207);
+        width: 100px;
+        height: 100%;
+        margin-right:16px ;
+    }
+    .content {
+        display:flex;
+        flex-direction: column;
+    }
+</style>
--- src/routes/testRoute/+page.svelte
+++ src/routes/testRoute/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to TestRoute</h1>
-<a href="/">Return to Main Page</a>
+<a href="/">Return to Main Page</a><br>
 <a href="/testRoute/deeperRoute">Enter a rabbit hole?</a>
